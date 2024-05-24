b109aae3b3b5b4bf00e97864eb9b2cece6f46e16
Aish Mishra <aishlunatic001@gmail.com>
2024-05-24 22:38:55 +0530

    java hello world and data types

---
+++ NonPrimitiveDT.java
@@ -0,0 +1,31 @@
+public class NonPrimitiveDT {
+  public static void main(String[] args){
+    // Non Primitive data types : 
+
+    // STRINGS -- 
+    String name1 = "Aman" ; 
+    System.out.println(name1.length()) ; 
+    String name2 = "Ram" ; 
+
+    // Concatitation : 
+    String name3 = name1 + " and "+ name2 ; 
+    System.out.println(name3) ; 
+
+
+// To find the position of a character in a string : 
+    System.out.println(name1.charAt(0)); 
+
+    // replace : 
+
+    // IN JAVA STRINGS ARE IMMUTABLE ---  That is why we have to store the replace char string in a new string  
+    String name4 = name1.replace('a','l');
+    System.out.println(name1) ; 
+
+    // substring : 
+
+    
+
+
+    
+  }
+}
---
+++ Variables.java
@@ -0,0 +1,26 @@
+public class Variables{
+
+  public static void main(String[] args){
+
+    // Primitive Data TYPES :------
+      // byte - 1 byte memory (-128 to 127)
+      // short - 2 bytes memory (-32768 to 32767)
+      // int - 4 bytes memory (-2147483648 to 2147483647)
+      // long - 8 bytes memory (-9223372036854775808 to 9223372036854775807)
+      // float - 4 bytes memory 
+      // double - 8 bytes memory 
+      // char - 2 bytes memory    a,b,c,@, # 
+      // boolean - 1 byte memory  true/false 
+
+      byte a = 4 ; 
+      System.out.println(a) ;
+      // byte b = 234324  ;
+      // System.out.println(b) ;  error due to number is too long to store 
+
+      float c = 3.45F ; 
+      System.out.println(c) ;
+
+      long d = 33423432432432L;
+      System.out.println(d) ;
+  }
+}
